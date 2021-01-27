ee57417e82d2fd3b084567a8f1894706aeadbc4f
milene.cruz <milene.cruz@leavening.com>
2021-01-27 16:07:11 -0300

    commit o commit

---
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -0,0 +1,71 @@
+class Api::V1::TodoItemsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @todo_item = current_user.todo_items.all
+  end
+
+  def show
+    if authorized?
+      respond_to do |format|
+        format.json { render :show }
+      end
+    else
+      handle_unauthorized
+    end
+
+  def create
+    @todo_item = current_user.todo_items.build(todo_item_params)
+    if authorized?
+      respond_to do |format|
+        if @todo_item.save
+          format.json { render :show, status: :created, location: api_v1_todo_path(@todo_item) }
+        else
+          format.json { render :show, status: :created, location: api_v1_todo_path(@todo_item) }
+        end
+      end
+    else  
+      handle_unauthorized
+    end            
+  end
+
+  
+  def update
+    if authorized?
+      respond_to do |format|
+        if @todo_item.update(todo_item_params)
+          format.json { render :show, status: :ok, location: api_v1_todo_item_path(@todo_item) }
+        else
+          format.json { render json: @todo_item.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+        handle_unauthorized
+    end
+  end
+
+  def destroy
+  end
+
+  private 
+    def set_todo_item
+      @todo_item = TodoItem.find(params[:id])
+    end
+
+    def autorized?
+      @todo_item.user == current_user
+    end
+
+    def handle_unauthorized
+      unless authorized?
+        respond_to do |format|
+          format.json { render :unauthorized, status: 401 }
+        end
+      end
+    end
+
+    def todo_item_params
+      params.require(:todo_item).permit(:title, :complete)
+    end
+end
